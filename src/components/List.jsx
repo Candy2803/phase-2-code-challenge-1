9756ae967013c3483745d1f3448fb2e948ca938b
+ <+>
2024-08-08 13:49:29 +0300

    modified the list component

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -5,7 +5,7 @@ const List = ({ transactions, onDelete }) => {
         <div className="overflow-x-auto mt-6 rounded-xl">
             <table className="min-w-full bg-white border border-gray-300 rounded-lg shadow-lg">
                 <thead>
-                    <tr className="bg-gray-700 text-white text-left">
+                    <tr className="bg-black text-white text-left">
                         <th className="py-3 px-4">Date</th>
                         <th className="py-3 px-4">Description</th>
                         <th className="py-3 px-4">Category</th>
@@ -15,7 +15,7 @@ const List = ({ transactions, onDelete }) => {
                 </thead>
                 <tbody>
                     {transactions.map((transaction) => (
-                        <tr key={transaction.id} className="border-t border-gray-300 brightness-70 bg-black text-white">
+                        <tr key={transaction.id} className="border-t border-gray-300 brightness-70 bg-gradient-to-r from-black to-violet-500 text-white">
                             <td className="py-3 px-4">{transaction.date}</td>
                             <td className="py-3 px-4">{transaction.description}</td>
                             <td className="py-3 px-4">{transaction.category}</td>
@@ -23,7 +23,7 @@ const List = ({ transactions, onDelete }) => {
                             <td className="py-3 px-4">
                                 <button 
                                     onClick={() => onDelete(transaction.id)} 
-                                    className="bg-purple-500 hover:bg-pink-700 text-white font-semibold py-1 px-3 rounded-lg shadow-lg transition duration-300"
+                                    className="bg-purple-500 hover:bg-pink-700 shadow-white text-white font-semibold py-1 px-3 rounded-lg shadow-lg transition duration-300"
                                 >
                                     Delete
                                 </button>
